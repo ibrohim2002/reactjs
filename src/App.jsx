daa138990a7280495e776749694982c57e31e0fc
ibrohim2002 <74803608+ibrohim2002@users.noreply.github.com>
2024-07-22 18:23:16 +0500

    useState, useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,43 @@
 import React from "react";
 import "./App.css";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log("constructor");
-    this.state = {
-      count: 0,
-    };
-    this.onChange = this.onChange.bind(this);
-  }
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(10);
 
-  componentWillMount() {
-    console.log("componentWillMount");
-  }
-
-  componentDidMount() {
-    console.log("componentDidMount");
-  }
-  componentWillUpdate() {
-    console.log("componentWillUpdate");
-  }
-  componentDidUpdate() {
-    console.log("componentDidUpdate");
-  }
-  componentWillUnmount() {
-    console.log("componentWillUnmount");
-  }
-
-  onChange = () => {
-    this.setState({ count: this.state.count + 1 });
+  const onChange = () => {
+    setCount(count + 1);
   };
-  render() {
-    console.log("render");
 
-    const onChange = () => {
-      this.setState({ count: this.state.count + 1 });
-      this.setState({ count: this.state.count + 1 });
-      this.setState({ count: this.state.count + 1 });
-    };
-    return (
-      <div>
-        <h1>Lifecycle</h1>
-        <h2>{this.state.count}</h2>
-        <button onClick={this.onChange}>click</button>
-      </div>
-    );
-  }
-}
+  const onChangePlus = () => {
+    setCount2(count2 + 1);
+  };
+
+  useEffect(() => {
+    console.log("effect 1");
+  });
+
+  useEffect(() => {
+    console.log("effect 2");
+  }, []);
+
+  useEffect(() => {
+    console.log("effect 3");
+  }, [count]);
+
+  useEffect(() => {
+    console.log("effect 4");
+  }, [count, count2]);
+
+  return (
+    <div>
+      <h1>Hooks</h1>
+      <h2>{count}</h2>
+      <h2>{count2}</h2>
+      <button onClick={onChange}>+</button>
+      <button onClick={onChangePlus}></button>
+    </div>
+  );
+};
 
 export default App;
